21079502a32a04035e5f2031f214203a152beb29
MANISHIMWESalton <manishimwejosephs@gmail.com>
2026-03-31 11:46:27 +0200

    parallel array

--- loops/maxAndMin.cpp
+++ loops/maxAndMin.cpp
@@ -5,6 +5,14 @@ int main(){
     int total = 0;
     int max=marks[0];
     int min = marks[0];
+    float average;
+
+    for(int i=0;i<5;i++){
+    total +=marks[i];
+    }
+
+    average = float (total)/5;
+
     for(int i = 1;i<5;i++){
         if(marks[i]>max){
             max = marks[i];
@@ -12,12 +20,22 @@ int main(){
         if(marks[i]<min){
             min = marks[i];
         }
-        total +=marks[i];
+        
     }
+    //parallel arrays array
+    
+    int id[3]={101,102,103};
+    int marks2[3]={80,75,90};
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "ID: " << id[i] << "  Marks: " << marks2[i] << "\n";
+    }
+    
     cout<<"The Total marks : "<< total <<endl;
     cout<<"The Maximum marks : "<< max <<endl;
     cout<<"The Minimum marks : "<< min <<endl;
+    cout<<"The Average marks : "<< average <<endl;
 
-
+return 0;
 
 }
