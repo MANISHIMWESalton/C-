343ae20dabaac39e4535b19db2fa86228b560446
MANISHIMWESalton <manishimwejosephs@gmail.com>
2026-04-13 08:17:36 +0200

    Naming

--- condition/condition.cpp
+++ condition/condition.cpp
@@ -6,7 +6,7 @@ int main(){
     cin>> marks;
     if(marks>90){
         cout<<"Distiction \n";
-    } else if (marks>=80 && marks>=50)
+    } else if (marks>=80 && marks<=50)
     {
         cout<<"Pass \n";
     } else
